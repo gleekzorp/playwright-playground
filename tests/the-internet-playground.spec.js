202853581e9a07c912ad2818de7046b5b3c8df2d
Daniel <gleek.dan@gmail.com>
2023-03-02 21:47:41 -0700

    Add: locator to show how to do a css selector

--- tests/the-internet-playground.spec.js
+++ tests/the-internet-playground.spec.js
@@ -3,6 +3,8 @@ import { test, expect } from "@playwright/test";
 test("Checkboxes", async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/");
   await page.getByRole("link", { name: "Checkboxes" }).click();
+
+  await expect(page.locator("#checkboxes input")).toHaveCount(2);
   const firstCheckbox = page.getByRole("checkbox").first();
   const secondCheckbox = page.getByRole("checkbox").nth(1);
 
